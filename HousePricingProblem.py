25d5096def15d47686e0c4d161356265d570d95c
William Nevada <59591636+AnNevada@users.noreply.github.com>
2023-02-15 14:32:20 +0700

    Add files via upload

---
+++ HousePricingProblem.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# Load data
+data = pd.read_csv("data.csv")
+
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+k = 5
+
+knn = KNeighborsRegressor(n_neighbors=k)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(y_test, y_pred)
+
+# Print the MSE and RMSE to evaluate the performance 
+print("MSE: {:.2f}".format(mse))
+print("RMSE: {:.2f}".format(rmse))
+print("MAE: {:.2f}".format(mae))
+
+
+# In[ ]:
+
+
+
+
