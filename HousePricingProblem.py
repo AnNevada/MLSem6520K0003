54e14a3d860e96cd81d5881b9c586da05c5d0535
William Nevada <59591636+AnNevada@users.noreply.github.com>
2023-02-15 14:43:01 +0700

    Add preprocessing

--- HousePricingProblem.py
+++ HousePricingProblem.py
@@ -9,12 +9,19 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.preprocessing import StandardScaler
 
 # Load data
 data = pd.read_csv("data.csv")
 
+#preprocessing data
+data.dropna(inplace=True)
+
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
