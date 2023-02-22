b0ad6a2faee0d32dd18ef65153c03cad68beb8ac
William Nevada <59591636+AnNevada@users.noreply.github.com>
2023-02-22 15:08:49 +0700

    Add files via upload

---
+++ AbaloneProblems/AbaloneMultinomialAndBenulli.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import KBinsDiscretizer
+import numpy as np
+
+data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data", header=None)
+
+data[0] = pd.Categorical(data[0]).codes
+
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+est = KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='uniform')
+X = est.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+# In[ ]:
+
+
+gau = GaussianNB()
+gau.fit(X_train, y_train)
+
+y_pred = gau.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+# In[ ]:
+
+
+be = BernoulliNB()
+
+be.fit(X_train, y_train)
+
+y_pred = be.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+# In[ ]:
+
+
+
+
